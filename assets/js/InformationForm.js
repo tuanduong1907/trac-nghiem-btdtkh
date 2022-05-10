279d2dedd671a4b2dc164a8e3286f61622d1de49
tuanduong1209 <tuanduong19077@gmail.com>
2022-05-10 09:33:28 +0700

    tuan update

--- assets/js/InformationForm.js
+++ assets/js/InformationForm.js
@@ -11,10 +11,13 @@ Validator({
   rules: [
     Validator.isRequired("#fullName", "Nhập họ tên của bạn"),
     Validator.minLength("#fullName", 6),
-    // Validator.isRequired("#birthday", "Nhập ngày sinh của bạn"),
+    Validator.isRequired("#birthday", "Nhập ngày sinh của bạn"),
     Validator.isRequired("#school", "Nhập tên trường của bạn"),
     Validator.isRequired("#class", "Nhập lớp của bạn"),
     Validator.isRequired("#address", "Nhập địa chỉ của bạn"),
+    Validator.isRequired("#phone", "Nhập số điện thoại của bạn"),
+    Validator.minLength("#phone", 10),
+
   ],
 
   onSubmit: function (data) {
